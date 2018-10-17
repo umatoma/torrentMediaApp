bc5fb103468eb5eba675c666b60ca297ff1ea2db
umatoma <umatomakun@gmail.com>
2018-10-17 22:09:39 +0900

    Introduce Babel and translate ES5 syntax into ES6 syntax.

--- serverApp/app.js
+++ serverApp/app.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import logger from 'morgan'
+import path from 'path'
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
 
-var app = express();
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+export default app
